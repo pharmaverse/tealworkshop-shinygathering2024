4ea7caa5746229a6699d958acadaffee2db7f55e
Dony Unardi <donyunardi@gmail.com>
2024-09-27 22:58:59 -0700

    add reporter

---
+++ 6-add-reporter.R
@@ -0,0 +1,103 @@
+library(teal)
+library(ggplot2)
+
+custom_module_ui <- function(id) {
+  ns <- NS(id)
+  
+  tags$div(
+    shiny::selectInput(
+      ns("datasets"),
+      label = "select dataset",
+      choices = c("iris", "mtcars")
+    ),
+    shiny::selectInput(
+      ns("variables"),
+      label = "select variable",
+      choices = NULL
+    ),
+    shiny::plotOutput(ns("my_plot")),
+    teal.widgets::verbatim_popup_ui(ns("rcode"), "Show R code"),
+    teal.reporter::simple_reporter_ui(ns("reporter"))
+  )
+  
+}
+
+custom_module_server <- function(id, data, reporter) {
+  moduleServer(id, function(input, output, session) {
+    
+    observeEvent(input$datasets, {
+      
+      only_numeric <- sapply(data()[[input$datasets]], is.numeric)
+      
+      updateSelectInput(
+        inputId = "variables",
+        choices = names(data()[[input$datasets]])[only_numeric]
+      )
+      
+    })
+    
+    result <- reactive({
+      req(input$datasets)
+      req(input$variables)
+      new_data <- within(
+        data(), {
+          my_plot <- ggplot(input_dataset, aes(x = input_vars)) +
+            geom_histogram(binwidth = 2, fill = "skyblue", color = "black")
+          my_plot
+        },
+        input_dataset = as.name(input$datasets),
+        input_vars = as.name(input$variables)        
+      )
+    })
+    
+    output$my_plot <- shiny::renderPlot({
+      result()[["my_plot"]]
+    })
+    
+    teal.widgets::verbatim_popup_srv(
+      id = "rcode",
+      verbatim_content = reactive(
+        teal.data::get_code(result())
+      ),
+      title = "Example Code"
+    )
+    
+    custom_function <- function(card = teal.reporter::ReportCard$new()) {
+      card$append_text(paste("Selected dataset", input$datasets))
+      card$append_plot(result()[["my_plot"]])
+    }
+    
+    teal.reporter::simple_reporter_srv(
+      id = "reporter",
+      reporter = reporter,
+      card_fun = custom_function
+    )    
+    
+  })
+}
+
+my_custom_module <- function(label = "My custom module") {
+  module(
+    label = label,
+    ui = custom_module_ui,
+    server = custom_module_server,
+    datanames = "all"
+  )
+}
+
+app <- init(
+  data = teal_data(
+    iris = iris,
+    mtcars = mtcars,
+    code = "
+      iris <- iris
+      mtcars <- mtcars
+    "
+  ),
+  modules = modules(
+    my_custom_module(label = "my module")
+  ),
+  header = "my teal app"
+)
+
+shinyApp(app$ui, app$server)
